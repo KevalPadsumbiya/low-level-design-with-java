f84f4133167032694d7885a2291c4deb917fe743
Keval Padsumbiya <68999502+KevalPadsumbiya@users.noreply.github.com>
2023-11-26 22:16:34 +0530

    Update ChatGPT.py

--- src/main/java/ChatGPT.py
+++ src/main/java/ChatGPT.py
@@ -1,15 +1,1 @@
-import os
-import openai
 
-openai.api_key = "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"
-
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
-  temperature=0.9,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0.0,
-  presence_penalty=0.6,
-  stop=[" Human:", " AI:"]
-)
